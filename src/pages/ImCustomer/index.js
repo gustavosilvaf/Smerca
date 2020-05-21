e0dce06681919ad47362f7ac88e4044a84bb339d
Gustavo Silva <5651627-gustavosilvaf@users.noreply.gitlab.com>
2020-05-21 14:08:17 +0000

    added and configured prettier and eslint

--- src/pages/ImCustomer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ImCustomer = () => {
-  return (
-    <h1>sMerca - Sou Cliente</h1>
-  );
-}
-
-export default ImCustomer;
--- src/pages/ImMarketer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ImMarketer = () => {
-  return (
-    <h1>sMerca - Sou Mercado</h1>
-  );
-}
-
-export default ImMarketer;
