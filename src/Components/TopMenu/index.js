1ddc067c5bd0ce2b9ccbfc9c90100c95f3cf65e3
Gustavo Silva <gustavofsilva@email.com>
2020-06-03 13:15:43 -0300

    added dropshadow on topMenu button and change code

--- src/Components/TopMenu/index.js
+++ src/Components/TopMenu/index.js
@@ -8,18 +8,11 @@ const TopMenu = () => (
     <img className="topMenu__logo" src="" alt="logo" />
     <nav className="topMenu__nav-container">
       {menuItens.map((item) => (
-        <a
-          className="topMenu__item"
-          href={item.link}
-          key={item.name}
-        >
+        <a className="topMenu__item" href={item.link} key={item.name}>
           {item.name}
         </a>
       ))}
-      <a
-        className="topMenu__button Button Button--white"
-        href="/teste"
-      >
+      <a className="topMenu__button Button Button--white" href="/teste">
         ENTRE EM CONTATO
       </a>
     </nav>
