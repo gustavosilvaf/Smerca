16a78099c0dbf3c13dacec93792e10aa6d921af4
Gustavo Silva <gustavofsilva@email.com>
2020-06-03 13:31:40 -0300

    changed className to BEM React rule

--- src/Components/TopMenu/index.js
+++ src/Components/TopMenu/index.js
@@ -4,15 +4,15 @@ import './styles.scss';
 import { menuItens } from '../../utils/constants';
 
 const TopMenu = () => (
-  <div className="topMenu">
-    <img className="topMenu__logo" src="" alt="logo" />
-    <nav className="topMenu__nav-container">
+  <div className="TopMenu">
+    <img className="TopMenu__logo" src="" alt="logo" />
+    <nav className="TopMenu__nav-container">
       {menuItens.map((item) => (
-        <a className="topMenu__item" href={item.link} key={item.name}>
+        <a className="TopMenu__item" href={item.link} key={item.name}>
           {item.name}
         </a>
       ))}
-      <a className="topMenu__button Button Button--white" href="/#">
+      <a className="TopMenu__button Button Button--white" href="/#">
         ENTRE EM CONTATO
       </a>
     </nav>
