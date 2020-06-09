4bc466be9e897c966d0feb5ecc6d59948f61ddf4
Gustavo Silva <gustavofsilva@email.com>
2020-06-09 10:45:24 -0300

    fixed images imports

--- src/Components/TopMenu/index.js
+++ src/Components/TopMenu/index.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import './styles.scss';
 
+import logoWhite from '../../assets/images/logo.svg';
+import menuButton from '../../assets/images/icons/menu-icon.png';
+
 import { menuItens } from '../../utils/constants';
 
 const TopMenu = () => {
   const [menuStats, setMenuStats] = useState(false);
 
-  const handleClick = () => setMenuStats(!menuStats)
+  const handleClick = () => setMenuStats(!menuStats);
 
   return (
     <div className="TopMenu">
@@ -15,9 +18,9 @@ const TopMenu = () => {
         onClick={handleClick}
         onKeyDown={handleClick}
         alt="menu-button"
-        src="/images/icons/menu-icon.png"
+        src={menuButton}
       />
-      <img className="TopMenu__logo" src="/images/logo.svg" alt="logo" />
+      <img className="TopMenu__logo" src={logoWhite} alt="logo" />
       <nav className={`TopMenu__nav-container ${menuStats ? '' : 'inactive'}`}>
         {menuItens.map((item) => (
           <a className="TopMenu__item" href={item.link} key={item.name}>
