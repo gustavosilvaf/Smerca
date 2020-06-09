f3c0792c9dae9206175c088aecabe089a895b9b8
Gustavo Silva <5651627-gustavosilvaf@users.noreply.gitlab.com>
2020-06-09 14:40:26 +0000

    Top menu

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Maintenance from './pages/Maintenance';
+import Home from './pages/Home';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Maintenance} />
+      <Route exact path="/home" component={Home} />
     </Switch>
   </BrowserRouter>
 );
