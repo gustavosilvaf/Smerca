6a680e7a47d5ebe69d2a49c1abd5f211917d9faf
Gustavo Silva <5651627-gustavosilvaf@users.noreply.gitlab.com>
2020-06-30 17:43:57 +0000

    created footer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './styles.scss';
 import TopMenu from '../../Components/TopMenu';
 import NearbyMarkets from '../../Components/Home/NearbyMarkets';
+import Footer from '../../Components/Footer';
 
 const Home = () => (
   <div className="Home">
     <TopMenu color="white" />
     <NearbyMarkets />
+    <Footer />
   </div>
 );
 
