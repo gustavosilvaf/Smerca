2c3536db13fc9a0d94f5217f6566bdfdaef3fb2a
Gustavo Silva <gustavofsilva@email.com>
2020-07-01 10:51:42 -0300

    created header on register store page

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Maintenance from './pages/Maintenance';
 import Home from './pages/Home';
 import Stores from './pages/Stores';
+import RegisterStore from './pages/RegisterStore';
 
 const Routes = () => (
   <BrowserRouter>
@@ -12,6 +13,7 @@ const Routes = () => (
       <Route exact path="/" component={Maintenance} />
       <Route exact path="/home" component={Home} />
       <Route exact path="/stores" component={Stores} />
+      <Route exact path="/register-store" component={RegisterStore} />
     </Switch>
   </BrowserRouter>
 );
