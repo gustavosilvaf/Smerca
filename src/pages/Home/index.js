8f3424afb0076e7b13c225c10e58960e19c996df
Gustavo Silva <gustavofsilva@email.com>
2020-07-01 09:26:45 -0300

    created contact container

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,6 +7,7 @@ import About from '../../Components/Home/About';
 import How from '../../Components/Home/How';
 import RegisterStore from '../../Components/Home/RegisterStore';
 import Doubt from '../../Components/Home/Doubt';
+import Contact from '../../Components/Home/Contact';
 
 const Home = () => (
   <div className="Home">
@@ -16,6 +17,7 @@ const Home = () => (
     <How />
     <RegisterStore />
     <Doubt />
+    <Contact />
     <Footer />
   </div>
 );
