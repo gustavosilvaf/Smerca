75c4e79ce920d1c669e8602bdce3cf213b7dba0c
Gustavo Silva <gustavofsilva@email.com>
2020-07-25 20:14:45 -0300

    added footer

--- src/pages/Stores/index.js
+++ src/pages/Stores/index.js
@@ -4,12 +4,14 @@ import './styles.scss';
 import TopMenu from '../../Components/TopMenu';
 import SearchContainer from '../../Components/Stores/SearchContainer';
 import StoresByTag from '../../Components/Stores/StoresByTag';
+import Footer from '../../Components/Footer';
 
 const Stores = () => (
   <>
     <TopMenu color="green" />
     <SearchContainer />
     <StoresByTag />
+    <Footer />
   </>
 );
 
