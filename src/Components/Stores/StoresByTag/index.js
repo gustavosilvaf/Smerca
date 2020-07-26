9c2950656ec4af4dde52545e1752a38819235a88
Gustavo Silva <gustavofsilva@email.com>
2020-07-26 15:01:09 -0300

    added carousel

--- src/Components/Stores/StoresByTag/index.js
+++ src/Components/Stores/StoresByTag/index.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../../Services/api';
 import './styles.scss';
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const StoresByTag = () => {
   const [stores, setStores] = useState([]);
@@ -27,7 +31,11 @@ const StoresByTag = () => {
       {stores.map((storeTag) => (
         <div key={storeTag.id} className="StoresByTag__tag-container">
           <h3 className="StoresByTag__tag-name">{storeTag.content}</h3>
-          <div className="StoresByTag__stores-display">
+          <Slider 
+          className="StoresByTag__stores-display"
+          dots={true}
+          slidesToShow={3}
+          >
             {storeTag.markets.data.markets.map((store) => (
               <div key={store.id} className="StoresByTag__store-container">
                 <div className="StoresByTag__store-image-crop">
@@ -40,7 +48,7 @@ const StoresByTag = () => {
                 <h3 className="StoresByTag__store-name">{store.name}</h3>
               </div>
             ))}
-          </div>
+          </Slider>
         </div>
       ))}
     </div>
